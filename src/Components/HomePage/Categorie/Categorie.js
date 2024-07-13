4d25171b5c4370bf6244faf2016f046f088f8707
unknown <Louis Pierre Andre>
2024-07-13 16:23:16 -0400

    Description de la modification

---
+++ src/Components/HomePage/Categorie/Categorie.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import './categorie.css'
+import img1 from './images/catego5.jpg'
+import img2 from './images/catego12.webp'
+import img3 from './images/catego10.jpg'
+import img4 from './images/catego6.jfif'
+
+function Categorie() {
+  return (
+    
+
+        < section class="top_categorie" >
+            <h2>Shop our top categories</h2>
+            <div>
+                <div>
+                    <img src={img1} alt="This is a shoes" />
+                    <h3>Addidas</h3>
+                </div>
+                <div>
+                    <img src={img2} alt="This is a shoes" />
+                    <h3>Nike</h3>
+                </div>
+                <div>
+                    <img src={img3} alt="This is a shoes" />
+                    <h3>Vans</h3>
+                </div>
+                <div>
+                    <img src={img4} alt="This is a shoes" />
+                    <h3>Converse</h3>
+                </div>
+                {/* <div>
+                    <img src={img5} alt="This is a shoes" />
+                    <h3>Nike</h3>
+              </div>
+              <div>
+                  <img src={img1} alt="This is a shoes" />
+                  <h3>Addidas</h3>
+              </div> */}
+
+
+            </div>
+        </section >
+
+  )
+}
+
+export default Categorie
--- src/Components/HomePage/Home.js
+++ src/Components/HomePage/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './Hero/Hero'
+import Categorie from './Categorie/Categorie'
 
 function Home() {
   return (
       <div>
-          <Hero />
+      <Hero />
+      <Categorie />
        </div>
   )
 }
